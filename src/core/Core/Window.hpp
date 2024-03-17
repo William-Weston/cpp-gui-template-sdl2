c5fb2f73972b8aed706ff04a93e142f143337db9
William Weston <wjtWeston@gmail.com>
2024-03-17 13:33:21 -0400

    updated formatting in Window class

--- src/core/Core/Window.hpp
+++ src/core/Core/Window.hpp
@@ -4,30 +4,33 @@
 
 #include <string>
 
-namespace App {
+namespace App
+{
 
-class Window {
- public:
-  struct Settings {
+class Window
+{
+public:
+  struct Settings
+  {
     std::string title;
-    int width{1280};
-    int height{720};
+    int width{ 1280 };
+    int height{ 720 };
   };
 
-  explicit Window(const Settings& settings);
+  explicit Window( const Settings& settings );
   ~Window();
 
-  Window(const Window&) = delete;
-  Window(Window&&) = delete;
-  Window& operator=(Window other) = delete;
-  Window& operator=(Window&& other) = delete;
+  Window( const Window& ) = delete;
+  Window( Window&& ) = delete;
+  Window& operator=( Window other ) = delete;
+  Window& operator=( Window&& other ) = delete;
 
   [[nodiscard]] SDL_Window* get_native_window() const;
   [[nodiscard]] SDL_Renderer* get_native_renderer() const;
 
- private:
-  SDL_Window* m_window{nullptr};
-  SDL_Renderer* m_renderer{nullptr};
+private:
+  SDL_Window* m_window{ nullptr };
+  SDL_Renderer* m_renderer{ nullptr };
 };
 
 }  // namespace App
